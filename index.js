abab1ec0b93f51f18e11ff1cb5903ec1f3c66916
RadiantCheddar <jonathanpei2004@gmail.com>
2020-07-17 14:11:14 -0400

    Server Ready

---
+++ index.js
@@ -0,0 +1,10 @@
+var PORT = process.env.PORT || 5000;
+var express = require('express');
+var app = express();
+var http = require('http');
+var server = http.Server(app);
+app.use(express.static('client'));
+server.listen(PORT, function () {
+  console.log('Running');
+});
+var io = require('socket.io')(server);
