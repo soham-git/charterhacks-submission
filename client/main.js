dc6fad0cb98571a43ad89a5e079c4de689fd9b7b
RadiantCheddar <jonathanpei2004@gmail.com>
2020-07-17 15:48:06 -0400

    lobbies

--- client/main.js
+++ client/main.js
@@ -1,9 +1,31 @@
 var socket = io();
 
-$('#test').click(function(){
-    socket.emit("message","")
+function showLobby(){
+    $("#lobby").show();
+    $("#singlePlayer").hide();
+    $("#multiPlayer").hide();
 
+}
+function showSinglePlayer(){
+    $("#lobby").hide();
+    $("#singlePlayer").show();
+    $("#multiPlayer").hide();
+}
+function showMultiPlayer(){
+    $("#lobby").hide();
+    $("#singlePlayer").hide();
+    $("#multiPlayer").show();
+}
+$('#createGame').click(function(){
+    var gamemode = document.getElementById("gamemode");
+    if(gamemode.value == "singleplayer"){
+        showSinglePlayer();
+    }
+    if(gamemode.value == "multiplayer"){
+        showMultiPlayer();
+    }
 });
+
 socket.on("message",function(data){
     alert(data);
 });
