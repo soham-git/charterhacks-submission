33d82255b4e228f6a788f35f94b7820d4a01dfae
RadiantCheddar <jonathanpei2004@gmail.com>
2020-07-18 18:52:50 -0400

    added questions

--- index.js
+++ index.js
@@ -3,15 +3,31 @@ var express = require('express');
 var app = express();
 var http = require('http');
 var server = http.Server(app);
+const CSVToJSON = require('csvtojson');
+
 app.use(express.static('client'));
 server.listen(PORT, function () {
     console.log('Running');
 });
 var io = require('socket.io')(server);
 
+var questions;
+CSVToJSON().fromFile('CharterHacks_Questions.csv')
+    .then(data => {
+
+        // users is a JSON array
+        // log the JSON array
+        questions = data;
+    }).catch(err => {
+        // log error if any
+        console.log(err);
+    });
+
+
 var games = [];
 var socketList = [];
 io.on('connection', function (socket) {
+
     socketList.push({ socket: socket, id: socket.id });
     io.emit('gameList', games);
 
@@ -20,7 +36,7 @@ io.on('connection', function (socket) {
         console.log(socket.id);
     })
     socket.on('createGame', function (data) {
-        games.push({ gameId: data.gameId, gameName: data.gameName, gameMode: data.gameMode, playerList: [socket.id], teams: [{players:[],question:-1,answer:-1,player:-1},{players:[],question:-1,answer:-1,player:-1}], started: false});
+        games.push({ gameId: data.gameId, gameName: data.gameName, gameMode: data.gameMode, playerList: [socket.id], teams: [{players:[],question:-1,answer:-1,player:-1},{players:[],question:-1,answer:-1,player:-1}], started: false,questionList:[]});
         socket.join(data.gameId);
         socket.emit('joinGame', { gameMode: "multi" });
         io.emit('gameList', games);
@@ -38,6 +54,7 @@ io.on('connection', function (socket) {
         }
     });
     socket.on('disconnect', function () {
+        console.log('pog');
         for (var i = 0; i < games.length; i++) {
             if (games[i].playerList.includes(socket.id)) {
                 for (var j = 0; j < games[i].playerList.length; j++) {
@@ -45,25 +62,20 @@ io.on('connection', function (socket) {
                         games[i].playerList.splice(j, 1);
                         j--;
                     }
-                    for (var j = 0; j < games[i].teams[0].players.length; j++) {
-                        if (games[i].teams[0].players[j] == socket.id) {
-                            games[i].teams[0].players.splice(j, 1);
-                            j--;
-                        }
-                    }
-                    for (var j = 0; j < games[i].teams[1].players.length; j++) {
-                        if (games[i].teams[1].players[j] == socket.id) {
-                            games[i].teams[1].players.splice(j, 1);
-                            j--;
-                        }
+                    
+                }
+                for (var j = 0; j < games[i].teams[0].players.length; j++) {
+                    if (games[i].teams[0].players[j] == socket.id) {
+                        games[i].teams[0].players.splice(j, 1);
+                        j--;
                     }
                 }
-            }
-        }
-        for (var i = 0; i < socketList.length; i++) {
-            if (socketList[i].id == socket.id) {
-                socketList.splice(i, 1);
-
+                for (var j = 0; j < games[i].teams[1].players.length; j++) {
+                    if (games[i].teams[1].players[j] == socket.id) {
+                        games[i].teams[1].players.splice(j, 1);
+                        j--;
+                    }
+                }
             }
         }
     });
@@ -140,18 +152,22 @@ function startGame(game) {
     var team1 = team2.splice(0, half)
     game.teams[0].players = team1;
     game.teams[1].players = team2;
-
-    startQuestion(game, 0, 1);
-    startQuestion(game, 1, 1);
+    game.questionList = [];
+    while(game.questionList.length < 8){
+        var r = Math.floor(Math.random() * 60) ;
+        if(game.questionList.indexOf(r) === -1) game.questionList.push(r);
+    }
+    startQuestion(game, 0, 0);
+    startQuestion(game, 1, 0);
     io.emit('gameList', games);
 
 }
 function startQuestion(game, team, questions) {
     for (var i = 0; i < game.teams[team].players.length; i++) {
-        io.to(game.teams[team].players[i]).emit('playerMessage', "Wait until it is your turn to answer.");
+        io.to(game.teams[team].players[i]).emit('playerMessage', "Wait until it is your turn to answer. "+game.questionList[questions]);
     }
     game.teams[team].player = game.teams[team].players[questions % (game.teams[team].players.length)];
-    io.to(game.teams[team].player).emit("playerMessage", "You are the current question answerer.");
+    io.to(game.teams[team].player).emit("playerMessage", "You are the current question answerer." +" "+game.questionList[questions]);
     var date = new Date();
     var startTime = date.getTime();
     var gameClock = setInterval(function () {
@@ -161,12 +177,16 @@ function startQuestion(game, team, questions) {
             io.to(game.teams[team].players[i]).emit('timer', (15000 - (curTime - startTime)) / 1000);
         }
         if (curTime - startTime > 15000) {
-            startQuestion(game, team, questions + 1);
+            if(questions+1<8){
+                startQuestion(game, team, questions + 1);
+            }
             clearInterval(gameClock);
         }
         else if (game.teams[team].answer != -1) {
             game.teams[team].answer = -1;
-            startQuestion(game, team, questions + 1);
+            if(questions+1<8){
+                startQuestion(game, team, questions + 1);
+            }
             clearInterval(gameClock);
         }
         
